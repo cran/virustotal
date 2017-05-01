d09a51f4c13ef414819a6d37445622ba95bde177
Gaurav Sood <gsood07@gmail.com>
2017-05-01 18:25:07 +0000

    version 0.2.1

--- R/virustotal.R
+++ R/virustotal.R
@@ -28,22 +28,23 @@ NULL
 #' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
 #' @return list
 
-virustotal_GET <- 
-function(query=list(), path = path, key = Sys.getenv("VirustotalToken"), ...) {
+virustotal_GET <- function(query=list(), path = path,
+                                     key = Sys.getenv("VirustotalToken"), ...) {
 
-	if (identical(key, "")) {
-        stop("Please set application id and password using set_key(key='key')).\n")
-	}
+  if (identical(key, "")) {
+        stop("Please set application key via set_key(key='key')).\n")
+  }
 
-	query$apikey <- key
+  query$apikey <- key
 
-	rate_limit()
+  rate_limit()
 
-	res <- GET("http://www.virustotal.com/", path = paste0("vtapi/v2/", path), query = query, ...)
-	virustotal_check(res)
-	res <- content(res)
+  res <- GET("http://www.virustotal.com/", path = paste0("vtapi/v2/", path),
+                                                             query = query, ...)
+  virustotal_check(res)
+  res <- content(res)
 
-	res
+  res
 }
 
 #'
@@ -56,22 +57,23 @@ function(query=list(), path = path, key = Sys.getenv("VirustotalToken"), ...) {
 #' @param \dots Additional arguments passed to \code{\link[httr]{POST}}.
 #' @return list
 
-virustotal_POST <- 
-function(query=list(), path = path, body=NULL, key = Sys.getenv("VirustotalToken"), ...) {
+virustotal_POST <- function(query=list(), path = path, body=NULL,
+                                     key = Sys.getenv("VirustotalToken"), ...) {
 
-	if (identical(key, "")) {
-        stop("Please set application id and password using set_key(key='key')).\n")
-	}
+  if (identical(key, "")) {
+        stop("Please set application key via set_key(key='key')).\n")
+  }
 
-	query$apikey <- key
+  query$apikey <- key
 
-	rate_limit()
+  rate_limit()
 
-	res <- POST("http://www.virustotal.com/", path = paste0("vtapi/v2/", path), query = query, body = body, ...)
-	virustotal_check(res)
-	res <- content(res)
+  res <- POST("http://www.virustotal.com/", path = paste0("vtapi/v2/", path),
+                                                query = query, body = body, ...)
+  virustotal_check(res)
+  res <- content(res)
 
-	res
+  res
 }
 
 #'
@@ -80,40 +82,48 @@ function(query=list(), path = path, body=NULL, key = Sys.getenv("VirustotalToken
 #' @param  req request
 #' @return in case of failure, a message
 
-virustotal_check <- 
-function(req) {
+virustotal_check <- function(req) {
 
-  if (req$status_code == 204) stop("Rate Limit Exceeded. Only 4 Queries per minute allowed.\n")
+  if (req$status_code == 204) stop("Rate Limit Exceeded.
+                                          Only 4 Queries per minute allowed.\n")
   if (req$status_code < 400) return(invisible())
 
   stop("HTTP failure: ", req$status_code, "\n", call. = FALSE)
-} 
+}
 
 #' 
 #' Rate Limits
 #' 
-#' Virustotal requests throttled at 4 per minute. This function creates an env. variable 
+#' Virustotal requests throttled at 4 per min. The function creates an env. var.
 #' that tracks number of requests per minute, and enforces appropriate waiting.
 #' 
 
 rate_limit <- function() {
 
-	# First request --- initialize time of first request and request count
-	if (Sys.getenv("VT_RATE_LIMIT") == "") return(Sys.setenv(VT_RATE_LIMIT = paste0(0, ",", Sys.time(), ",", 0)))
+  # First request --- initialize time of first request and request count
+  if (Sys.getenv("VT_RATE_LIMIT") == "") {
+    return(Sys.setenv(VT_RATE_LIMIT = paste0(0, ",", Sys.time(), ",", 0)))
+  }
 
-	rate_lim         <- Sys.getenv("VT_RATE_LIMIT") 
-	req_count        <- as.numeric(gsub(",.*", "", rate_lim)) + 1
-	past_duration    <- as.numeric(strsplit(rate_lim, ",")[[1]][3], units="secs")	
-	current_duration <- difftime(Sys.time(), as.POSIXct(strsplit(rate_lim, ",")[[1]][2]), units = "secs") 
+  rate_lim         <- Sys.getenv("VT_RATE_LIMIT")
+  req_count        <- as.numeric(gsub(",.*", "", rate_lim)) + 1
+  past_duration    <- as.numeric(strsplit(rate_lim, ",")[[1]][3],
+                                                                 units = "secs")
+  current_duration <- difftime(Sys.time(),
+                    as.POSIXct(strsplit(rate_lim, ",")[[1]][2]), units = "secs")
 
-	if (current_duration > 60) return(Sys.setenv(VT_RATE_LIMIT = paste0(1, ",", Sys.time(), ",", 0)))
+  if (current_duration > 60) {
+    return(Sys.setenv(VT_RATE_LIMIT = paste0(1, ",", Sys.time(), ",", 0)))
+  }
 
-	net_duration     <- past_duration + current_duration
+  net_duration     <- past_duration + current_duration
 
-	if (req_count > 4 & net_duration <= 60) { 
-		Sys.sleep(60 -  net_duration)
-		return(Sys.setenv(VT_RATE_LIMIT = paste0(1, ",", Sys.time(), ",", 0)))
-	}
+  if (req_count > 4 & net_duration <= 60) {
 
-	return(Sys.setenv(VT_RATE_LIMIT = paste0(req_count, ",", Sys.time(), ",", net_duration)))
+    Sys.sleep(60 -  net_duration)
+    return(Sys.setenv(VT_RATE_LIMIT = paste0(1, ",", Sys.time(), ",", 0)))
+  }
+
+  return(Sys.setenv(VT_RATE_LIMIT =
+                         paste0(req_count, ",", Sys.time(), ",", net_duration)))
 }
