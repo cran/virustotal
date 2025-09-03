2e14995ea7f19980b2e7d667d29827337f6dd2a9
Gaurav Sood <gsood07@gmail.com>
2025-09-03 00:20:02 +0000

    version 0.3.0

--- inst/doc/using_virustotal.R
+++ inst/doc/using_virustotal.R
@@ -1,31 +1,64 @@
-## ---- eval=F, install---------------------------------------------------------
-#  #library(devtools)
-#  install_github("soodoku/virustotal")
+## ----eval=F, install----------------------------------------------------------
+# # Install from CRAN
+# install.packages("virustotal")
+# 
+# # Or install development version
+# # library(devtools)
+# # install_github("themains/virustotal")
 
-## ---- eval=F, load------------------------------------------------------------
-#  library(virustotal)
+## ----eval=F, load-------------------------------------------------------------
+# library(virustotal)
 
-## ---- eval=F, api_key---------------------------------------------------------
-#  set_key("your_key")
+## ----eval=F, api_key----------------------------------------------------------
+# set_key("your_api_key_here")
 
-## ---- eval=F,  domain---------------------------------------------------------
-#  domain_report("http://www.google.com")$categories
+## ----eval=F, scan_file--------------------------------------------------------
+# # Submit a file for analysis
+# result <- scan_file("path/to/suspicious_file.exe")
+# analysis_id <- result$data$id
 
-## ---- eval=F, scan_url--------------------------------------------------------
-#  scan_url("http://www.google.com")
+## ----eval=F, file_report------------------------------------------------------
+# # Get analysis results using file hash
+# report <- file_report("99017f6eebbac24f351415dd410d522d")
+# 
+# # Access scan results
+# scan_results <- report$data$attributes$last_analysis_results
+# total_engines <- length(scan_results)
+# detections <- sum(sapply(scan_results, function(x) x$category == "malicious"))
 
-## ---- eval=F, url-------------------------------------------------------------
-#  head(url_report("http://www.google.com")[, 1:2], 10)
+## ----eval=F, rescan_file------------------------------------------------------
+# # Request new analysis of existing file
+# rescan_result <- rescan_file("99017f6eebbac24f351415dd410d522d")
+# new_analysis_id <- rescan_result$data$id
 
-## ---- eval=F, ip--------------------------------------------------------------
-#  ip_report("8.8.8.8")$country
+## ----eval=F, scan_url---------------------------------------------------------
+# # Submit URL for analysis
+# url_result <- scan_url("http://suspicious-site.com")
+# analysis_id <- url_result$data$id
 
-## ---- eval=F, file------------------------------------------------------------
-#  head(file_report("99017f6eebbac24f351415dd410d522d")[,1:2], 10)
+## ----eval=F, url_report-------------------------------------------------------
+# # Get analysis results using URL
+# report <- url_report("http://www.google.com")
+# 
+# # Access scan results
+# scan_results <- report$data$attributes$last_analysis_results
+# threat_score <- report$data$attributes$stats
 
-## ---- eval=F, scan_file-------------------------------------------------------
-#  scan_file("using_virustotal.Rmd")[,1:2]
+## ----eval=F, domain-----------------------------------------------------------
+# # Get comprehensive domain analysis
+# domain_info <- domain_report("google.com")
+# 
+# # Access various data points
+# categories <- domain_info$data$attributes$categories
+# whois_data <- domain_info$data$attributes$whois
+# dns_records <- domain_info$data$attributes$dns_records
 
-## ---- eval=F, rescan_file-----------------------------------------------------
-#  rescan_file(hash='99017f6eebbac24f351415dd410d522d')[,1:2]
+## ----eval=F, ip---------------------------------------------------------------
+# # Get IP analysis including geolocation and ASN
+# ip_info <- ip_report("8.8.8.8")
+# 
+# # Access geo and network information
+# country <- ip_info$data$attributes$country
+# asn <- ip_info$data$attributes$asn
+# network <- ip_info$data$attributes$network
 
